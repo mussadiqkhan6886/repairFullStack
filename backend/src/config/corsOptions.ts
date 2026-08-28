03fbd1b4f685038fc8a40240ea2adc527acc258c
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-08-28 21:20:26 +0500

    cors jobe done

---
+++ backend/src/config/corsOptions.ts
@@ -0,0 +1,12 @@
+import { CorsOptions } from "cors"
+import { allowedOrigins } from "./allowedOrigins"
+
+export const corsOptions : CorsOptions = {
+    origin: (origin : string | undefined, callback: (err : null | Error, allow?: boolean) => void) => {
+        if(!origin || allowedOrigins.includes(origin)){
+            callback(null, true)
+            return
+        }
+        callback(new Error ("Origin error cors not allowed"))
+    }
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import { corsOptions } from "./config/corsOptions"
 
 dotenv.config()
-
 const app = express()
 const PORT = process.env.PORT || 4000
 
-app.get("/", (req, res) => {
-    res.send("Hello mr")
-})
+app.use(cors(corsOptions))
+app.use(cookieParser())
+app.use(express.json())
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
