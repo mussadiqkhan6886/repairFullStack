ee32e941ae83a9ddb1553e0413af0baa4c45e981
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-08-29 15:19:13 +0500

    completed

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -1,7 +1,13 @@
+/// <reference path="../types/express.d.ts" />
 import { Request, Response } from "express";
 import User from "../models/UserModel";
 
 export const getAllUsers = async (req: Request, res: Response) : Promise<void> => {
+
+    if(req?.user?.role === "Employee"){
+        res.status(401).json({message: "Unauthorized"})
+        return
+    }
     const users = await User.find({}).select("-password").lean().exec()
 
     res.status(200).json({
