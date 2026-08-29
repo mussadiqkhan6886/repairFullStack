cf3711fb3c3329838ffce64df6b90707dfd912ea
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-08-29 22:32:53 +0500

    starting query

--- backend/src/schemas/userSchema.ts
+++ backend/src/schemas/userSchema.ts
@@ -11,17 +11,13 @@ export type userZod = z.infer<typeof userSchema>
 
 export const updateUserSchema = z.object({
     username: z.string().min(3).optional(),
-
     email: z.string().email().optional(),
-
     password: z.string().min(4).trim().optional(),
-
     role: z.enum([
         "Employee",
         "Manager",
         "Admin"
     ]).optional(),
-
     status: z.enum([
         "Active",
         "InActive"
