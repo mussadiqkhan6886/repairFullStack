a91cdad91328ecb61c5be8f08e581099f8d43e06
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-09-01 01:06:18 +0500

    note fetching done

--- frontend/server/note.ts
+++ frontend/server/note.ts
@@ -1,16 +1,37 @@
-export const getAllNotes = async () => {
+import { fetchHelper } from "@/lib/helpers/fetchHelper"
 
+export const getAllNotes = async () : Promise<NoteType[]> => {
+    const result = await fetchHelper<{note: NoteType[]}>("notes")
+    return result.note
 }
 
 
-export const getNote = async () => {
-
-}
-
-export const updateNote = async (data: string) => {
-
-}
-
-export const deleteNote = async () => {
+export const getNote = async (id: string) : Promise<NoteType> => {
+    const result = await fetchHelper<{note: NoteType}>(`notes/${id}`)
+    return result.note
     
+}
+
+export const updateNote = async (data: updateNoteData) : Promise<NoteType> => {
+    const result = await fetchHelper<{note: NoteType}>(`notes/${data.id}`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    })
+
+    return result.note
+}
+
+export const createNote = async (data: Required<NoteType>) : Promise<NoteType> => {
+    const result = await fetchHelper<{note: NoteType}>("notes", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+    })
+
+    return result.note
+}
+
+export const deleteNote = async (id: string) : Promise<void> => {
+    await fetchHelper<undefined>(`notes/${id}`, {method: "DELETE", headers: {"Content-Type": "application/json"}})
 }
